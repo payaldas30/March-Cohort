fe42ae938108e4018022a1173dddc5df46285506
T. Teja <tteja375@gmail.com>
2025-03-24 22:09:30 +0530

    mcp working fine here

--- Backend/models/User.js
+++ Backend/models/User.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const UserSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+  chatHistory: [{ type: Object }]
 });
 
 module.exports = mongoose.model("User", UserSchema);
